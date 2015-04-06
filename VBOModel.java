24ac535ad670ef4d19898a5785823e094b8a4364
Anthony Granger <grangeranthony@gmail.com>
2015-04-06 13:32:20 +0200

    Termine de corriger le calcul des normales par vertex \o/

--- VBOModel.java
+++ VBOModel.java
@@ -5,6 +5,7 @@ import com.jogamp.common.nio.PointerBuffer;
 import com.jogamp.common.nio.Buffers;
 import java.nio.DoubleBuffer;
 import java.nio.IntBuffer;
+import java.util.ArrayList;
 
 public class VBOModel {
 
@@ -26,11 +27,11 @@ public class VBOModel {
     public VBOModel(GL2 gl, int nVertices, int nTriangles) {
         vertices = new double[nVertices*3];
         edges = new int[nTriangles*3];
-        normals = new double[nTriangles*3];
+        normals = new double[nVertices*3];
 
         //Pour debug
-        normalsLineVert = new double[nTriangles*6];
-        normalsLine = new int[nTriangles*2];
+        normalsLineVert = new double[nVertices*6];
+        normalsLine = new int[nVertices*2];
 
         build();
         initVBO(gl);
@@ -104,7 +105,7 @@ public class VBOModel {
         gl.glVertexPointer(3, GL2.GL_DOUBLE, 0, 0);
 
         gl.glBindBuffer(GL2.GL_ELEMENT_ARRAY_BUFFER, normalsLineVBO);
-        gl.glDrawElements(GL2.GL_LINES, 40, GL2.GL_UNSIGNED_INT, 0);
+        gl.glDrawElements(GL2.GL_LINES, normalsLine.length, GL2.GL_UNSIGNED_INT, 0);
         /**************************/
 
         gl.glDisableClientState(GL2.GL_VERTEX_ARRAY);
@@ -117,6 +118,8 @@ public class VBOModel {
     public void computeNormals() {
 
         double[] normalsFaces = new double[edges.length];
+        double[] normalVertex = new double[3];
+        ArrayList<Integer> posVertex = new ArrayList<Integer>();
         double[] result = new double[3];
         double[] v1 = new double[3];
         double[] v2 = new double[3];
@@ -125,7 +128,9 @@ public class VBOModel {
         double[] tmp2 = new double[3];
         double length = 0.0;
 
-        /**Normales par face**/
+        /****************
+        Normales par face
+        ****************/
         for (int i=0; i<edges.length; i+=3) {
             //remplir les tableaux
             for (int j=0; j<3; j++) {
@@ -136,6 +141,9 @@ public class VBOModel {
 
             //faire le calcul de la normale
             for (int j=0; j<3; j++) {
+
+                //TODO : supprimer
+                /**HOT FIX pour l'isocaèdre**/
                 if (i < 15 || (i > 30 && i%6!=0)) {
                     tmp1[j] = v1[j] - v2[j];
                     tmp2[j] = v1[j] - v3[j];
@@ -144,6 +152,7 @@ public class VBOModel {
                     tmp1[j] = v1[j] - v3[j];
                     tmp2[j] = v1[j] - v2[j];
                 }
+                /****************************/
             }
 
             result[0] = tmp1[1] * tmp2[2] - tmp1[2] * tmp2[1];
@@ -152,39 +161,77 @@ public class VBOModel {
 
             //"normaliser" la normale
             length = result[0]*result[0] +
-                            result[1]*result[1] +
-                            result[2]*result[2];
+                result[1]*result[1] +
+                result[2]*result[2];
+
             if (length != 0)
                 length = 1.0 / Math.sqrt(length);
 
-            for (int j=0; j<3; j++) {
+            for (int j=0; j<3; j++)
                 result[j] *= length;
-            }
 
             //stocker
             for (int j=0; j<3; j++) {
-                normals[i+j] = result[j];
+                normalsFaces[i+j] = result[j];
             }
         }
 
+        /******************
+        Normales par vertex
+        ******************/
+        for (int i=0; i<vertices.length; i+=3) {
+            //remise à zéro:
+            for (int j=0; j<3; j++)
+                normalVertex[j] = 0.0;
+            posVertex.clear();
+
+            //recherche à quelles faces appartient le vertex
+
+            for (int j=0; j<edges.length; j+=3) {
+                if (edges[j]==i/3 ||
+                    edges[j+1]==i/3 ||
+                    edges[j+2]==i/3)
+                    posVertex.add(j/3);
+            }
+
+            //on fait la moyenne de toutes les normales
+            for (int normalIndex : posVertex) {
+                normalVertex[0] += normalsFaces[normalIndex*3];
+                normalVertex[1] += normalsFaces[normalIndex*3+1];
+                normalVertex[2] += normalsFaces[normalIndex*3+2];
+            }
+
+            normalVertex[0] /= posVertex.size();
+            normalVertex[1] /= posVertex.size();
+            normalVertex[2] /= posVertex.size();
+
+            //on stocke
+            normals[i] = normalVertex[0];
+            normals[i+1] = normalVertex[1];
+            normals[i+2] = normalVertex[2];
+        }
+
+        for (int i=0; i<normals.length; i+=3)
+            System.out.println(normals[i]+";"+normals[i+1]+";"+normals[i+2]);
 
         /**Génération de l'affichage des normales  : **/
         //for (int i=60; i<edges.length*2; i+=6) {
-        for (int i=0; i<edges.length*2; i+=6) {
-            normalsLineVert[i] = vertices[edges[i/2]*3];
-            normalsLineVert[i+1] = vertices[edges[i/2]*3+1];
-            normalsLineVert[i+2] = vertices[edges[i/2]*3+2];
+        for (int i=0; i<normals.length*2; i+=6) {
+            normalsLineVert[i] = vertices[i/2];
+            normalsLineVert[i+1] = vertices[(i/2)+1];
+            normalsLineVert[i+2] = vertices[(i/2)+2];
 
-            normalsLineVert[i+3] = vertices[edges[i/2]*3] + normals[i/2] * normDispLen;
-            normalsLineVert[i+4] = vertices[edges[i/2]*3+1] + normals[(i/2)+1] * normDispLen;
-            normalsLineVert[i+5] = vertices[edges[i/2]*3+2] + normals[(i/2)+2] * normDispLen;
+            normalsLineVert[i+3] = vertices[i/2] + normals[i/2] * normDispLen;
+            normalsLineVert[i+4] = vertices[(i/2)+1] + normals[(i/2)+1] * normDispLen;
+            normalsLineVert[i+5] = vertices[(i/2)+2] + normals[(i/2)+2] * normDispLen;
         }
 
         for (int i=0; i<normalsLine.length; i++) {
             normalsLine[i] = i;
         }
 
-        //for (int i=0; i<normalsLine.length; i++)
-        //System.out.println(normalsLineVert[normalsLine[0]*3]+";"+normalsLineVert[normalsLine[1]*3]);
+        System.out.println("--------------------");
+        for (int i=0; i<normalsLineVert.length; i+=3)
+            System.out.println(normalsLineVert[i]+";"+normalsLineVert[i+1]+";"+normalsLineVert[i+2]);
     }
 }
